b98f0e4f5b2874c9d454b074bb339dfb31f0d2ba
Mubashir Chaudhary <Mchaudhary5949@conestogac.on.ca>
2019-11-13 15:19:02 -0500

    ....

--- test1/index.js
+++ test1/index.js
@@ -4,14 +4,14 @@ Survey
     .applyTheme("default");
 
 var json = {
-    title: "American History",
-    pages: [
+    title: "Reference Check Questionaire",
+    /*pages: [
         {
             questions: [
                 {
                     type: "radiogroup",
-                    name: "civilwar",
-                    title: "When was the Civil War?",
+                    name: "What makes the candidate a good fit for this job?",
+                    title: "What makes the candidate a good fit for this job?",
                     choices: [
                         "1750-1800", "1800-1850", "1850-1900", "1900-1950", "after 1950"
                     ],
@@ -44,7 +44,145 @@ var json = {
                     correctAnswer: "The foundation of the British parliamentary system"
                 }
             ]
-        }
+        }*/
+            "pages": [
+             {
+              "name": "page1",
+              "elements": [
+               {
+                "type": "radiogroup",
+                "name": "Question 1",
+                "title": "How long have you known this person? ",
+                "choices": [
+                 {
+                  "value": "item1",
+                  "text": "less than 12 months"
+                 },
+                 {
+                  "value": "item2",
+                  "text": "1-2 years"
+                 },
+                 {
+                  "value": "item3",
+                  "text": "Over 3 years"
+                 }
+                ]
+               },
+               {
+                "type": "text",
+                "name": "Question 2",
+                "title": "What is your relationship with this applicant?",
+                "description": "specify exact relationship"
+               }
+              ]
+             },
+             {
+              "name": "page2",
+              "elements": [
+               {
+                "type": "comment",
+                "name": "Question 3",
+                "title": "Do you know of any reason we might not want this person to work with people with mental or physical disabilities? If yes,  what is that reason? "
+               },
+               {
+                "type": "comment",
+                "name": "Question 4",
+                "title": "Have you ever worked on a project with this individual? If yes, did the applicant\nfollow through with his/her portion of the duties or responsibilities?"
+               }
+              ]
+             },
+             {
+              "name": "page3",
+              "elements": [
+               {
+                "type": "radiogroup",
+                "name": "Question 5",
+                "title": "How would you rate the quality of this applicant’s contribution to the project?",
+                "choices": [
+                 {
+                  "value": "item1",
+                  "text": "Excellent"
+                 },
+                 {
+                  "value": "item2",
+                  "text": "Good"
+                 },
+                 {
+                  "value": "item6",
+                  "text": "Average"
+                 },
+                 {
+                  "value": "item4",
+                  "text": "Not Enough"
+                 },
+                 {
+                  "value": "item5",
+                  "text": "No Contribution"
+                 }
+                ]
+               },
+               {
+                "type": "comment",
+                "name": "Question 6",
+                "title": "How would you best describe this individual as?"
+               }
+              ]
+             },
+             {
+              "name": "page4",
+              "elements": [
+               {
+                "type": "text",
+                "name": "Question 7",
+                "title": "Does this individual ever had any issues with the team or related work?"
+               },
+               {
+                "type": "radiogroup",
+                "name": "Question 8",
+                "title": "Is there any disciplinary actions taken against the applicant?",
+                "choices": [
+                 {
+                  "value": "item1",
+                  "text": "Yes"
+                 },
+                 {
+                  "value": "item2",
+                  "text": "No"
+                 }
+                ]
+               }
+              ]
+             },
+             {
+              "name": "page5",
+              "elements": [
+               {
+                "type": "text",
+                "name": "Question 9",
+                "title": "Is he able to handle conflict?"
+               },
+               {
+                "type": "dropdown",
+                "name": "Question 10",
+                "title": "Is there anything else you would like to add about (candidate)?",
+                "choices": [
+                 {
+                  "value": "item1",
+                  "text": "Yes"
+                 },
+                 {
+                  "value": "item2",
+                  "text": "No"
+                 },
+                 {
+                  "value": "item3",
+                  "text": "Maybe"
+                 }
+                ]
+               }
+              ]
+             }
+           
     ],
     completedHtml: "<h4>You have answered correctly <b>{correctedAnswers}</b> questions from <b>{questionCount}</b>.</h4>"
 };
--- test2/single.js
+++ test2/single.js
@@ -4,8 +4,8 @@ Survey
     .applyTheme("default");
 
 var json = {
-    title: "American History",
-    pages: [{
+    title: " Reference Check Questionaire",
+    /*pages: [{
         questions: [
             {
                 type: "radiogroup",
@@ -37,7 +37,90 @@ var json = {
                 correctAnswer: "The foundation of the British parliamentary system"
             }
         ]
-    }],
+    }*/
+        "pages": [
+         {
+          "name": "page1",
+          "elements": [
+           {
+            "type": "comment",
+            "name": "Question 1",
+            "title": "What makes the candidate a good fit for this job?",
+            "description": "Briefly describe please."
+           },
+           {
+            "type": "text",
+            "name": "Question 2",
+            "title": "What are the candidate’s biggest strengths and weaknesses?"
+           },
+           {
+            "type": "text",
+            "name": "Question 3",
+            "title": "How do you know the job candidate?"
+           },
+           {
+            "type": "text",
+            "name": "Question 4",
+            "title": "Can you verify the job candidate’s employment, job title, pay, and responsibilities? Why did they leave that job?"
+           },
+           {
+            "type": "radiogroup",
+            "name": "Question 5",
+            "title": "If you had the opportunity, would you re-hire this job candidate? Why?",
+            "choices": [
+             {
+              "value": "item1",
+              "text": "Yes"
+             },
+             {
+              "value": "item2",
+              "text": "No"
+             }
+            ]
+           },
+           {
+            "type": "radiogroup",
+            "name": "Question 6",
+            "title": "Did the candidate get along with their co-workers and management?",
+            "choices": [
+             {
+              "value": "item1",
+              "text": "Yes"
+             },
+             {
+              "value": "item2",
+              "text": "No"
+             }
+            ]
+           },
+           {
+            "type": "comment",
+            "name": "Question 7",
+            "title": "Tell me what it’s like to work with the job candidate.",
+            "description": "Mention some good qualities"
+           },
+           {
+            "type": "text",
+            "name": "Question 8",
+            "title": "What advice can you give me to successfully manage the job candidate?"
+           },
+           {
+            "type": "text",
+            "name": "Question 9",
+            "title": "What else do I need to know about the job candidate that I didn’t already ask?"
+           },
+           {
+            "type": "comment",
+            "name": "Question 10",
+            "title": "Who else should I speak to about the candidate that can provide different insight?",
+            "description": "You can mention names and phone numbers."
+           }
+          ]
+         },
+         {
+          "name": "page2"
+         }
+        ],
     completedHtml: "<h4>You have answered correctly <b>{correctedAnswers}</b> questions from <b>{questionCount}</b>.</h4>"
 };
 
